09a53ad0986ffbf1aba34bf63447a8daeeb72811
rafeeque386 <rafeeque386@gmail.com>
2020-10-29 20:01:56 +0530

    react redux form revisited error solved

--- src/redux/configurestore.js
+++ src/redux/configurestore.js
@@ -4,20 +4,24 @@ import { Promotions } from './promotions';
 import { Leaders } from './leaders';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
+
 
 import {createStore, combineReducers, applyMiddleware } from 'redux';
 
 
-export const ConfigureStore =()=>{
-    const store =createStore(
+export const ConfigureStore = () => {
+    const store = createStore(
         combineReducers({
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders  
+            leaders: Leaders,
+            ...createForms({feedback: InitialFeedback})
         }),
         applyMiddleware(thunk, logger)
-
     );
+
     return store;
-}
+};
